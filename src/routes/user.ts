1350b7bea551191926bb6b008615a1f415506824
erik-ferreirra <erikdesouzagf@gmail.com>
2024-06-24 22:24:47 -0300

    create route to upload file

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,24 @@
+import fs from "node:fs"
+import path from "node:path"
 import { ZodError } from "zod"
+import { promisify } from "node:util"
+import { pipeline } from "node:stream"
+import { randomUUID } from "node:crypto"
 import { FastifyInstance } from "fastify"
+import fastifyMultipart from "@fastify/multipart"
 
 import { prisma } from "../lib/prisma"
 import { createUserBodySchema } from "../schemas/user"
 
+const pump = promisify(pipeline)
+
 export async function userRoutes(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1_048_576 * 100, // 100mb
+    },
+  })
+
   // list all users
   app.get("/users", async (_, reply) => {
     const users = await prisma.user.findMany()
@@ -12,6 +26,7 @@ export async function userRoutes(app: FastifyInstance) {
     return reply.status(200).send({ users })
   })
 
+  // create a new user
   app.post("/users", async (request, reply) => {
     try {
       const { name } = createUserBodySchema.parse(request.body)
@@ -28,9 +43,32 @@ export async function userRoutes(app: FastifyInstance) {
         message = error?.issues[0].message
       }
 
-      console.log(error)
-
       reply.code(400).send({ message })
     }
+  })
+
+  app.post("/users/upload", async (request, reply) => {
+    const data = await request.file()
+
+    if (!data) {
+      return reply.status(400).send({ error: "Missing file input" })
+    }
+
+    const extension = path.extname(data.filename)
+
+    if (extension !== ".mp4") {
+      return reply
+        .status(400)
+        .send({ error: "Invalid file type, please upload a MP4." })
+    }
+
+    const fileBaseName = path.basename(data.filename, extension)
+    const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
+
+    const uploadDestination = path.resolve(__dirname, "../tmp", fileUploadName)
+
+    await pump(data.file, fs.createWriteStream(uploadDestination))
+
+    return reply.send({ message: "Upload de arquivo" })
   })
 }
