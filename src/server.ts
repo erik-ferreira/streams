3e56456f3329ac9187966e01e901a318a822a60d
erik-ferreirra <erikdesouzagf@gmail.com>
2024-06-24 21:18:07 -0300

    update ignore

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import fastify from "fastify"
 
+import { userRoutes } from "./routes/user"
+
 const app = fastify()
+
+app.register(userRoutes)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running")
